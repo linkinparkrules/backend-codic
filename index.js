1b67e0dd670705cc5118768d6c7f970d91b1517a
linkinparkrules <nghiacao1997@gmail.com>
2022-10-20 17:54:19 +0700

    test res.data again

--- index.js
+++ index.js
@@ -10,23 +10,23 @@ app.use(cors({
     origin: "http://localhost:3000"
 }))
 
-const jwt = require('jsonwebtoken');
-app.post('/login', (req,res) => {
-    const username = req.body.username;
-    const token = jwt.sign(
-        {username: username},
-        "myPrivateKey",
-        {expiresIn: 3600}
-    )
-    res.json({
-        username:username,
-        token:token
-    })
-});
+// const jwt = require('jsonwebtoken');
+// app.post('/login', (req,res) => {
+//     const username = req.body.username;
+//     const token = jwt.sign(
+//         {username: username},
+//         "myPrivateKey",
+//         {expiresIn: 3600}
+//     )
+//     res.json({
+//         username:username,
+//         token:token
+//     })
+// });
 
-// connectToDb();
+connectToDb();
 
-// app.use(router);
+app.use(router);
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
--- routers/user.js
+++ routers/user.js
@@ -2,9 +2,9 @@ const express = require('express');
 const userRouter = express.Router();
 const {login} = require('../controllers/userController.js');
 
-userRouter.post('/login', (req,res) => {
+userRouter.post('/login', async (req,res) => {
     try {
-        const user = login(req.body.username, req.body.password);
+        const user = await login(req.body.username, req.body.password);
         res.json(user);
     } catch (err) {
         res.status(409).send(err.message);
