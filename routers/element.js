3454dc98c45034af977a5d4a1c5fd370e196e6da
linkinparkrules <nghiacao1997@gmail.com>
2022-10-31 00:02:06 +0700

    add err message for element router

--- routers/element.js
+++ routers/element.js
@@ -3,14 +3,18 @@ const elementRouter = express.Router();
 const {showTagType} = require('../controllers/elementController');
 
 elementRouter.get('/exercise/element', async (req,res) => {
-    const htmlTag = await showTagType("html");
-    const cssTag  = await showTagType("css");
-    const jsTag   = await showTagType("js");
-    res.json({
-        htmlTag: htmlTag,
-        cssTag: cssTag,
-        jsTag: jsTag
-    });
+    try {
+        const htmlTag = await showTagType("html");
+        const cssTag  = await showTagType("css");
+        const jsTag   = await showTagType("js");
+        res.json({
+            htmlTag: htmlTag,
+            cssTag: cssTag,
+            jsTag: jsTag
+        });
+    } catch(err) {
+        res.status(404).send(err.message);
+    }
 });
 
 module.exports = elementRouter;
