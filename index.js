4febeccccb92da00582bc0ec0d93f88c8f96b9b8
linkinparkrules <nghiacao1997@gmail.com>
2023-03-10 02:06:15 +0700

    fix port

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const router = require('./routers');
 const {connectToDb} = require('./database');
 const cors = require('cors');
 
-// const port = process.env.PORT;
-const port = 5001;
+const port = process.env.PORT;
+// const port = 5001;
 app.use(express.json());
 
 app.options('*', cors())
