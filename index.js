208e42cc3c21f1c3e3efdf44f4e4f542586c8c99
linkinparkrules <nghiacao1997@gmail.com>
2023-03-10 01:52:34 +0700

    fix cors

--- index.js
+++ index.js
@@ -4,20 +4,32 @@ const router = require('./routers');
 const {connectToDb} = require('./database');
 const cors = require('cors');
 
-const port = process.env.PORT;
-// const port = 5001;
+// const port = process.env.PORT;
+const port = 5001;
 app.use(express.json());
 
+app.options('*', cors())
 const whitelist = ["http://localhost:3000", "https://codic.vercel.app", "http://localhost:5001"];
 const corsOptions = {
     origin: function (origin, callback) {
       if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
+        callback(null, false)
       } else {
         callback(new Error('Not allowed by CORS'))
       }
     }
 }
+
+app.use(function(req, res, next) {
+  // Set the Access-Control-Allow-Origin header to allow requests from any domain
+  res.header("Access-Control-Allow-Origin", "*");
+  // Set the Access-Control-Allow-Methods header to allow POST and GET requests
+  res.header("Access-Control-Allow-Methods", "POST, GET");
+  // Set the Access-Control-Allow-Headers header to allow requests with the "Content-Type" header
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  // Call the next middleware in the chain
+  next();
+});
 
 app.use(cors(corsOptions));
 
