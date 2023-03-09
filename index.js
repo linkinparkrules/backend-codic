697d53d8b2dad49e77c791781ccb20293e6afa98
linkinparkrules <nghiacao1997@gmail.com>
2023-03-10 01:31:31 +0700

    fix cors

--- index.js
+++ index.js
@@ -8,19 +8,18 @@ const port = process.env.PORT;
 // const port = 5001;
 app.use(express.json());
 
-// const whitelist = ["http://localhost:3000", "https://codic.vercel.app", "http://localhost:5001"];
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//       if (whitelist.indexOf(origin) !== -1) {
-//         callback(null, true)
-//       } else {
-//         callback(new Error('Not allowed by CORS'))
-//       }
-//     }
-// }
+const whitelist = ["http://localhost:3000", "https://codic.vercel.app", "http://localhost:5001"];
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+}
 
-// app.use(cors(corsOptions));
-app.use(cors())
+app.use(cors(corsOptions));
 
 connectToDb();
 
